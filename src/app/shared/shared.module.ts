db77a3677c4ecc85d5a76feef0c57562f9efc670
Yiqiao Liao <joeglancith@hotmail.com>
2018-02-08 17:08:27 -0500

    Added shared Module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { 
+  MatToolbarModule, 
+  MatIconModule, 
+  MatButtonModule, 
+  MatCardModule,
+  MatInputModule,
+  MatSidenavModule,
+  MatListModule,
+} from '@angular/material';
+
+const MATERIAL_MODULES = [
+  MatToolbarModule, 
+  MatIconModule, 
+  MatButtonModule, 
+  MatCardModule,
+  MatInputModule,
+  MatSidenavModule,
+  MatListModule,
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ...MATERIAL_MODULES,
   ],
   exports: [
-    CommonModule
+    CommonModule,
+    ...MATERIAL_MODULES,
   ],
   declarations: []
 })
