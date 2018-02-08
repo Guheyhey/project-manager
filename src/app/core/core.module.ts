11a430469bdc7b46b9343cb53a90d8a9cbb12714
Yiqiao Liao <joeglancith@hotmail.com>
2018-02-08 17:09:16 -0500

    Added core module inclueding header, footer and sidebar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,21 @@
 import { NgModule, SkipSelf, Optional } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { SharedModule } from '../shared/shared.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { loadSvgResource } from '../utils/svg.util';
+
 
 @NgModule({
   imports: [
-    CommonModule
+    HttpModule,
+    SharedModule,
+    BrowserAnimationsModule
   ],
   declarations: [
     HeaderComponent, 
@@ -20,9 +29,14 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   ]
 })
 export class CoreModule { 
-  constructor(@Optional() @SkipSelf() parent: CoreModule) {
+  constructor(
+    @Optional() @SkipSelf() parent: CoreModule,
+    ir: MatIconRegistry, 
+    ds: DomSanitizer
+  ) {
     if (parent) {
       throw new Error('Module already exist, Cannot load again');
     }
+    loadSvgResource(ir, ds)
   }
 }
