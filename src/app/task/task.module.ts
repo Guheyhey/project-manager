e1823e9b5dca3da5d9dccdad008856f17fd10785
Yiqiao Liao <joeglancith@hotmail.com>
2018-02-18 14:51:07 -0500

    Declared NewTaskComponent and CopyTaskComponent in the task module

--- src/app/task/task.module.ts
+++ src/app/task/task.module.ts
@@ -7,6 +7,7 @@ import { TaskHeaderComponent } from './task-header/task-header.component';
 import { SharedModule } from '../shared/shared.module';
 import { TaskRoutingModule } from './task-routing.module';
 import { NewTaskComponent } from './new-task/new-task.component';
+import { CopyTaskComponent } from './copy-task/copy-task.component';
 
 @NgModule({
   imports: [
@@ -18,10 +19,12 @@ import { NewTaskComponent } from './new-task/new-task.component';
     TaskListComponent, 
     TaskItemComponent, 
     TaskHeaderComponent, 
-    NewTaskComponent
+    NewTaskComponent, 
+    CopyTaskComponent
   ],
   entryComponents: [
-    NewTaskComponent
+    NewTaskComponent,
+    CopyTaskComponent
   ]
 })
 export class TaskModule { }
