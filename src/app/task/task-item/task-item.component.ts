6b6e0cec9bcb140c01da6d90033b2a97904d29e3
Yiqiao Liao <joeglancith@hotmail.com>
2018-02-18 16:11:59 -0500

    Added dialog to all buttons in tasklist page

--- src/app/task/task-item/task-item.component.ts
+++ src/app/task/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-task-item',
@@ -8,11 +8,23 @@ import { Component, OnInit, Input } from '@angular/core';
 export class TaskItemComponent implements OnInit {
   @Input() item;
   @Input() avatar;
+  @Output() taskClick = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit() {
     this.avatar = this.item.owner ? this.item.owner.avatar : 'unassigned';
+  }
+
+  onItemClick() {
+    this.taskClick.emit();
+  }
+
+  onCheckBoxClick(ev: Event) {
+    // 阻止事件处理的传播
+    // 即点击事件在此处被处理之后，不会发送到父组件被继续处理
+    // 父组件的click 就不会触发
+    ev.stopPropagation();
   }
 
 }
--- src/app/task/task.module.ts
+++ src/app/task/task.module.ts
@@ -8,6 +8,7 @@ import { SharedModule } from '../shared/shared.module';
 import { TaskRoutingModule } from './task-routing.module';
 import { NewTaskComponent } from './new-task/new-task.component';
 import { CopyTaskComponent } from './copy-task/copy-task.component';
+import { NewTaskListComponent } from './new-task-list/new-task-list.component';
 
 @NgModule({
   imports: [
@@ -20,10 +21,12 @@ import { CopyTaskComponent } from './copy-task/copy-task.component';
     TaskItemComponent, 
     TaskHeaderComponent, 
     NewTaskComponent, 
-    CopyTaskComponent
+    CopyTaskComponent, 
+    NewTaskListComponent
   ],
   entryComponents: [
     NewTaskComponent,
+    NewTaskListComponent,
     CopyTaskComponent
   ]
 })
